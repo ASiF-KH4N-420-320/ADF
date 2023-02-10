1dfa28a42582db2a5f53fc995de0ad35c5a868f2
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2023-02-11 01:36:14 +0500

    Update Adf.py

--- Adf.py
+++ Adf.py
@@ -1,5 +1,15 @@
-if __name__ == "__main__":
-   try:
-       __import__("ADF").reg()
-   except Exception as e:
-       exit(str(e))
+import os, platform
+
+def Run():
+
+        bit = platform.architecture()[0]
+
+        if bit == '64bit':
+
+            import ADF
+
+        elif bit == '32bit':
+
+            import ADF
+
+Run()
